865da660df168696634c367cdacc9b5aa14612f0
zipzonanimation-spec <zipzonanimation@gmail.com>
2026-07-04 22:30:52 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,25 +1,19 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
-android {
-    namespace = "com.yourgame.videoexport"
-    compileSdk = 34
-    defaultConfig {
-        minSdk = 21
-        targetSdk = 34
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.4.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
     }
 }
 
-dependencies {
-    compileOnly("org.godotengine:godot:4.2.1.stable")
-    implementation("androidx.core:core-ktx:1.9.0")
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven { url = uri("https://maven.godotengine.org/") }
+    }
 }
