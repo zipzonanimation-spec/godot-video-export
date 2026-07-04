6948b40f399dbc9a1987781066a9ae848f9b4f39
zipzonanimation-spec <zipzonanimation@gmail.com>
2026-07-04 22:11:53 +0800

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,3 @@ plugins {
     id("com.android.library") version "7.4.2" apply false
     id("org.jetbrains.kotlin.android") version "1.8.0" apply false
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
